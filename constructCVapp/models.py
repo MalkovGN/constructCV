35018d23688b4fde26c987f9f5e7b902d84ab928
MalkovGN <malkovgn@gmail.com>
2022-07-24 14:01:43 +0300

    Addingg new fields and some css

--- constructCVapp/models.py
+++ constructCVapp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
 
 class infoCVModel(models.Model):
     firstName = models.CharField(max_length=64)
     secondName = models.CharField(max_length=64)
+    emailAdress = models.EmailField(blank=True, null=True)
+    phoneValidate = RegexValidator(regex=r'^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$')
+    phoneNumber = models.CharField(max_length=12, blank=True, null=True, validators=[phoneValidate])
+    gitHubLink = models.URLField(blank=True, null=True)
+    socialContacts = models.CharField(max_length=64, blank=True, null=True)
+    wantedJobTitle = models.CharField(max_length=64, null=True)
     fileCV = models.FileField(upload_to='', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
--- constructCVapp/views.py
+++ constructCVapp/views.py
@@ -20,6 +20,7 @@ from decimal import Decimal
 from .models import infoCVModel
 from .forms import infoCVForm
 
+
 def mainPage(request):
     return render(request, 'constructCVapp/mainPage.html')
 
@@ -77,7 +78,6 @@ def logOutUser(request):
         return redirect('mainPage')
 
 
-
 def createCV(request):
     if request.method == 'GET':
         return render(request, 'constructCVapp/createCV.html', {'form': infoCVForm()})
@@ -86,10 +86,31 @@ def createCV(request):
         if form.is_valid():
             firstName = form.cleaned_data.get('firstName')
             secondName = form.cleaned_data.get('secondName')
+            emailAdress = form.cleaned_data.get('emailAdress')
+            phoneNumber = form.cleaned_data.get('phoneNumber')
+            gitHubLink = form.cleaned_data.get('gitHubLink')
+            socialContacts = form.cleaned_data.get('socialContacts')
+            wantedJobTitle = form.cleaned_data.get('wantedJobTitle')
+        else:
+            return render(
+                request,
+                'constructCVapp/createCV.html',
+                {'form': infoCVForm(), 'error': 'Please, enter a correct phone number!'}
+            )
+        # Checking emptiness of fields
+        if emailAdress is None:
+            emailAdress = ''
+        if phoneNumber is None:
+            phoneNumber = ''
+        if gitHubLink is None:
+            gitHubLink = ''
+        if socialContacts is None:
+            socialContacts = ''
 
         pdf = Document()
         page = Page()
         pdf.add_page(page)
+
         r: Rectangle = Rectangle(
             Decimal(400),
             Decimal(848 - 84 - 100),
@@ -97,7 +118,31 @@ def createCV(request):
             Decimal(150),
         )
         page.add_annotation(SquareAnnotation(r, stroke_color=HexColor('#ffffff')))
-        Paragraph(firstName + ' ' + secondName, horizontal_alignment=Alignment.CENTERED).layout(page, r)
+        Paragraph(
+            str(emailAdress) + '\n' + str(phoneNumber) + '\n' + str(gitHubLink) + '\n' + str(socialContacts),
+            horizontal_alignment=Alignment.CENTERED
+        ).layout(page, r)
+
+        r: Rectangle = Rectangle(
+            Decimal(50),
+            Decimal(785),
+            Decimal(340),
+            Decimal(40),
+        )
+        Paragraph(
+            firstName + ' ' + secondName,
+            font_size=Decimal(22),
+            vertical_alignment=Alignment.MIDDLE).layout(page, r)
+        page.add_annotation(SquareAnnotation(r, stroke_color=HexColor('#ffffff')))
+
+        r: Rectangle = Rectangle(
+            Decimal(50),
+            Decimal(770),
+            Decimal(340),
+            Decimal(15),
+        )
+        Paragraph(wantedJobTitle, font_size=Decimal(10), horizontal_alignment=Alignment.LEFT).layout(page, r)
+        page.add_annotation(SquareAnnotation(r, stroke_color=HexColor('#ffffff')))
 
         with open(Path(f'constructCVapp/static/constructCVapp/{firstName}{secondName}CV.pdf'), 'wb') as new_pdf:
             PDF.dumps(new_pdf, pdf)
